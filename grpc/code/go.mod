bcb477623cf3b7b248822faca0f483dd353f13d5
dajimenezriv <dajimenezriv@gmail.com>
2026-09-24 14:17:56 +0200

    Commit

--- grpc/code/go.mod
+++ grpc/code/go.mod
@@ -3,13 +3,23 @@ module grpc
 go 1.26.5
 
 require (
+	go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc v0.71.0
+	go.opentelemetry.io/otel v1.46.0
+	go.opentelemetry.io/otel/sdk v1.46.0
+	go.opentelemetry.io/otel/trace v1.46.0
 	google.golang.org/grpc v1.84.0
 	google.golang.org/protobuf v1.36.12
 )
 
 require (
-	golang.org/x/net v0.57.0 // indirect
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/go-logr/logr v1.4.4 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	go.opentelemetry.io/auto/sdk v1.2.1 // indirect
+	go.opentelemetry.io/otel/metric v1.46.0 // indirect
+	golang.org/x/net v0.58.0 // indirect
 	golang.org/x/sys v0.47.0 // indirect
-	golang.org/x/text v0.40.0 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20260706201446-f0a921348800 // indirect
+	golang.org/x/text v0.41.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20260825221802-da73d73af1c5 // indirect
 )
--- grpc/code/main.go
+++ grpc/code/main.go
@@ -7,6 +7,11 @@ import (
 	"net"
 	"time"
 
+	"go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/propagation"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	"go.opentelemetry.io/otel/trace"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
@@ -14,12 +19,16 @@ import (
 const addr = ":8000"
 
 func main() {
+	otel.SetTracerProvider(sdktrace.NewTracerProvider())
+	otel.SetTextMapPropagator(propagation.TraceContext{})
+
 	lis, err := net.Listen("tcp", addr)
 	if err != nil {
 		panic(err)
 	}
 
-	s := grpc.NewServer()
+	// Server side: reads `traceparent` from the metadata and puts the span in ctx.
+	s := grpc.NewServer(grpc.StatsHandler(otelgrpc.NewServerHandler()))
 	proto.RegisterAlertsServer(s, &server{})
 
 	go func() {
@@ -30,16 +39,24 @@ func main() {
 
 	time.Sleep(time.Second)
 
-	conn, err := grpc.NewClient(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	// Client side: starts a child span and writes `traceparent` into the metadata.
+	conn, err := grpc.NewClient(addr,
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.WithStatsHandler(otelgrpc.NewClientHandler()))
 	if err != nil {
 		panic(err)
 	}
 	defer conn.Close()
 
+	// Add the traceID to the context.
+	ctx, span := otel.Tracer("client").Start(context.Background(), "main")
+	defer span.End()
+	fmt.Println("client trace_id:", span.SpanContext().TraceID())
+
 	// The connection is lazy, until we don't do the first request it doesn't connect.
 	// If the connection fails it will retry with backoff.
 	client := proto.NewAlertsClient(conn)
-	res, err := client.GetAlert(context.Background(), &proto.GetAlertRequest{Id: 1})
+	res, err := client.GetAlert(ctx, &proto.GetAlertRequest{Id: 1})
 	if err != nil {
 		panic(err)
 	}
@@ -54,6 +71,7 @@ type server struct {
 }
 
 func (s *server) GetAlert(ctx context.Context, in *proto.GetAlertRequest) (*proto.GetAlertResponse, error) {
+	fmt.Println("server trace_id:", trace.SpanContextFromContext(ctx).TraceID())
 	return &proto.GetAlertResponse{
 		Alert: &proto.Alert{Id: in.GetId()},
 		Ok:    true}, nil
